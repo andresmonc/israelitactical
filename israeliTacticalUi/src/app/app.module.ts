68eefb7790564f7dcfdede57dcecb1d5dce6d1fa
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-11-03 18:30:46 -0500

    Created placeholder carousel

--- israeliTacticalUi/src/app/app.module.ts
+++ israeliTacticalUi/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
