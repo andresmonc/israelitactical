ed25487bb0527e168cf7a82f11e7f7542778cbea
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-11-20 09:24:34 -0600

    Form passes api link to console

--- israeliTacticalUi/src/app/app.module.ts
+++ israeliTacticalUi/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { CarouselComponent } from './carousel/carousel.component';
@@ -28,7 +31,9 @@ import { RequirementsComponent } from './requirements/requirements.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- israeliTacticalUi/src/app/contact/contact.component.ts
+++ israeliTacticalUi/src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../models/contact.model';
+import { Http, Response, Headers, RequestOptions } from '@angular/http'; 
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  baseUrl = environment.baseUrl;
+  constructor(private http: Http) { }
+  private serverApi = this.baseUrl;
 
   ngOnInit() {
   }
-
+  onSubmit(contact: Contact) {
+    // this.http.get(`${serverApi}/mailer/?name=${contact.name}&email=jaimeamonc@gmail.com&message=hellooooo`).subscribe(status => console.log(JSON.stringify(status)));
+    console.log(`${this.serverApi}/mailer/?name=${contact.name}&email=${contact.email}&message=${contact.message}`);
+  }
 }
