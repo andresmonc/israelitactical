746554fc44415b8c2e9a7c0aaac0723415cabb26
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-12-30 15:08:29 -0600

    enhanced contact form to hide form on submit

--- israeliTacticalUi/src/app/contact/contact.component.ts
+++ israeliTacticalUi/src/app/contact/contact.component.ts
@@ -10,6 +10,8 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
+  public showMessage: boolean = false;
+  public showForm: boolean = true;
   public contact: Contact = {
     name: '',
     email: '',
@@ -20,14 +22,16 @@ export class ContactComponent implements OnInit {
   private serverApi = this.baseUrl;
 
   ngOnInit() {
+
   }
 
   onSubmit(form: NgForm) {
-    console.log(this.contact.name.length)
     if (this.contact.name.length == 0 || this.contact.email.length == 0 || this.contact.message.length == 0) {
       alert("Please fill out all fields!");
     } else {
       this.http.get(`${this.serverApi}/mailer/?name=${this.contact.name}&email=${this.contact.email}&message=${this.contact.message}`).subscribe(status => alert("Message has been sent!"));
+      this.showForm = false;
+      this.showMessage = true; 
     }
   }
 
