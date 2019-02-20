d697a0396bfeca04f7f16517c802f4821b8af7ea
Jaime Moncayo <jaimeandresmonc@gmail.com>
2019-02-19 18:21:56 -0600

    removed contact form

--- israeliTacticalUi/src/app/contact/contact.component.ts
+++ israeliTacticalUi/src/app/contact/contact.component.ts
@@ -25,16 +25,6 @@ export class ContactComponent implements OnInit {
 
   }
 
-  onSubmit(form: NgForm) {
-    if (this.contact.name.length == 0 || this.contact.email.length == 0 || this.contact.message.length == 0) {
-      alert("Please fill out all fields!");
-    } else {
-      this.http.get(`${this.serverApi}/mailer/?name=${this.contact.name}&email=${this.contact.email}&message=${this.contact.message}`).subscribe(response => {
-        this.showForm = false;
-        this.showMessage = true; 
-      });
 
-    }
-  }
 
 }
